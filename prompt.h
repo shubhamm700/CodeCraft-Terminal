c719df02883326b15659d3aa7a7ecb7532ff840c
shubham <shubam.198sk@gmail.com>
2022-12-27 11:45:39 +0530

    Added cd files

---
+++ prompt.h
@@ -0,0 +1,10 @@
+#ifndef __PROMPT_H
+#define __PROMPT_H
+
+// These functions are used to print the prompt
+void printInPrompt();
+void welcomePrompt();
+int ifAddTelda();
+void updateRelativePathToMove();
+
+#endif
